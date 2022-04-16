4cba9ab867a9d5715c53e0e98f0796b0dc7999e2
ABViney <vineyadrian@gmail.com>
2022-04-15 21:23:50 -0400

    algorithm prototype

--- src/com/cell/LinkedPath.java
+++ src/com/cell/LinkedPath.java
@@ -2,11 +2,14 @@ package com.cell;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 
 import com.directional.Direction;
 import com.directional.Vec2;
@@ -21,146 +24,157 @@ import com.vindig.image.Color;
 public class LinkedPath {
 	
 	
-	public LinkedPath(AbstractCellComponent[][] sheet, Set<List<Vec2>> pathSet, int newCellWidth, int newCellHeight, int xChannels, int yChannels) {
+	public static void linkPaths(AbstractCellComponent[][] sheet, Set<List<Vec2>> pathSet, int newCellWidth, int newCellHeight, int xChannels, int yChannels) {
+//		System.out.println("Group");
+//		for(List<Vec2> vector : pathSet) {
+//			System.out.println("\tVector");
+//			for(Vec2 v : vector) {
+//				System.out.println("\t\t"+Arrays.toString(v.getOrigin()));
+//			}
+//			
+//		}
 		for(List<Vec2> path : pathSet) {
-			
+			System.out.println("PathSize : " +path.size());
+			System.out.println("Start: " + Arrays.toString(path.get(path.size()-1).getOrigin()) + "\nEnd: " + Arrays.toString(path.get(0).getOrigin()));
 			Vec2 coord = path.get(path.size()-1); // coord is "end" of path. This position denotes the location of the NodeCellComponent this path will socket into
-			Vec2 nextCoord = path.get(path.size()-2); // The amount of Vec2's in any path is at minimum 2
-			int index = 3;
+			int index = 1;
 
-			PathInstance curPath = new PathInstance((NodeCellComponent)sheet[coord.getY()][coord.getX()]); // All NodeCells should be instantiated before this sheet is recieved
+			PathInstance curPath = new PathInstance((NodeCellComponent)sheet[coord.getY()][coord.getX()]); // All NodeCells should be instantiated before this sheet is received
 			
-			Direction compass = coord.directionTo(nextCoord);
 			
-			PathCellComponent lastPathCell;
-			List<Entry<Direction, List<PathInstance[][]>>> vectors = new ArrayList<>();
-			
-			Function<PathCellComponent, PathInstance[][]> getChannels;
-			
-			switch(compass) {
-			case UP, DOWN:
-				getChannels = pcc -> pcc.verticalScan;
-			case LEFT, RIGHT:
-				getChannels = pcc -> pcc.horizontalScan;
-			default:
-				getChannels = pcc -> null;
-			}
-			
+			List<Entry<Direction, PathInstance[][][]>> vectors = new ArrayList<>();
+			System.out.println("Group");
 			do {
-				Vec2 incr = Vec2.fromDirection(compass);
-				List<PathInstance[]> vector = new ArrayList<>();
+				Vec2 nextCoord = path.get(path.size()-1 - index++);
+				Direction compass = coord.directionTo(nextCoord);
+				
+				Function<PathCellComponent, PathInstance[][]> getChannels;
+				switch(compass) {
+				case UP,DOWN:
+					getChannels = (pcc -> pcc.verticalScan);
+					break;
+				case LEFT,RIGHT:
+					getChannels = pcc -> pcc.horizontalScan;
+					break;
+				default:
+					getChannels = pcc -> null;
+				}
+				System.out.println("\tValids instantiated");
 				List<PathCellComponent> valids = new LinkedList<>();
+				Vec2 incr = Vec2.fromDirection(compass);
+				
+				System.out.println("Coord: " +Arrays.toString(coord.getOrigin()));
+				System.out.println("NextCoord" +Arrays.toString(nextCoord.getOrigin()));
+				
+				switch(compass) {
+				case UP:
+					System.out.println("UP");
+					break;
+				case DOWN:
+					System.out.println("DOWN");
+					break;
+				case LEFT:
+					System.out.println("LEFT");
+					break;
+				case RIGHT:
+					System.out.println("RIGHT");
+				}
+				
 				while(!coord.equals(nextCoord)) { // Get the PathCellComponents between coord and nextCoord
 					coord = coord.add(incr);
+					System.out.println("Incremented: " + Arrays.toString(coord.getOrigin()));
 					if(!(sheet[coord.getY()][coord.getX()] instanceof NodeCellComponent)) {
-						if(!(sheet[coord.getY()][coord.getX()] instanceof PathCellComponent)) // add pathcomponents as necessary
+						if(!(sheet[coord.getY()][coord.getX()] instanceof PathCellComponent)) // instantiate pathcomponents as necessary
 							sheet[coord.getY()][coord.getX()] = new PathCellComponent(newCellWidth, newCellHeight, xChannels, yChannels);
 						valids.add((PathCellComponent)sheet[coord.getY()][coord.getX()]);
 					}
 				}
+				vectors.add(new SimpleEntry<>(compass,
+						valids.stream().map(getChannels).toArray(PathInstance[][][]::new)));
 				
-				
-				vectors.add(new SimpleEntry<>(compass, valids.stream().map(getChannels).toList()));
-				
-				nextCoord = path.get(path.size() - index++);
-				compass = coord.directionTo(nextCoord);
 			} while(index < path.size());
 			
+			int[] indexGroup = new int[vectors.size()];
 			
-			boolean pathFound = false;
-			int startingOffset = 0;
-			while(!pathFound) {
+			PATHFINDER:
+			while(true) {
 				
-				Direction lastCompass = vectors.get(0).getKey();
-				PathInstance[][] lastChannel = null;
 				for(int i = 0; i < vectors.size(); i++) {
-					int cd = 0; // Channel Descriptor (x, y)
-					switch(lastCompass) {
-					case UP,DOWN:
-						cd = yChannels;
+					int[] accessOrder;
+					boolean accessToggle = false;
+					int indexLimit = 0;
+					switch(vectors.get(i).getKey()) {
+					case UP, LEFT:
+						accessOrder = new int[] {1, 0};
+						indexLimit = yChannels-1;
 						break;
-					case LEFT,RIGHT:
-						cd = xChannels;
+					case DOWN, RIGHT:
+						accessOrder = new int[] {0, 1};
+						indexLimit = xChannels-1;
 						break;
+					default:
+						accessOrder = null;
+					}
+					if(accessOrder == null) {
+						System.out.println("Error PATHFINDER");
+						break PATHFINDER;
 					}
 					
-					List<PathInstance[][]> valRef = vectors.get(i).getValue();
-					
-					PathInstance[][][] channelBundle = valRef.toArray(new PathInstance[vectors.get(i).getValue().get(0).length][2][cd]);
+					PathInstance[][][] instances = vectors.get(i).getValue();
+					Arrays.stream(instances).forEach(inst -> System.out.println(inst.length));
+					System.out.println(i + "/" + vectors.size() + " Instances Length: " + instances.length);
+					boolean unobstructed = true;
+					for(int c = 0; c < instances.length-1; c++) { // -1 leave last for next view
+						
+						if(instances[c][((accessToggle = !accessToggle) ? accessOrder[0] : accessOrder[1])][indexGroup[i]] != null) { // accessOrder puts the right array in the right order
+							indexGroup[i]++; //increment current index
+							unobstructed = false; // attempt is obstructed
+							for(int j = i; j < indexGroup.length; j++) {
+								if(indexGroup[i] >= indexLimit) {
+									if(i < indexGroup.length) {
+										indexGroup[i+1]++;
+										indexGroup[i] = 0;
+									} else {
+										System.out.println("Error indexGroup overflow");
+										break PATHFINDER;
+									}
+								}
+							}
+						}
+						if(!unobstructed) continue PATHFINDER;
+					}
 				}
 				
+				for(int i = 0; i < vectors.size(); i++) {
+					int[] accessOrder;
+					boolean accessToggle = false;
+					switch(vectors.get(i).getKey()) {
+					case UP, LEFT:
+						accessOrder = new int[] {1, 0};
+						break;
+					case DOWN, RIGHT:
+						accessOrder = new int[] {0, 1};
+						break;
+					default:
+						accessOrder = null;
+					}
+					if(accessOrder == null) {
+						System.out.println("Error PATHFINDER");
+						break PATHFINDER;
+					}
+					
+					PathInstance[][][] instances = vectors.get(i).getValue();
+					System.out.println(" Length " + instances.length);
+					
+					for(int c = 0; c < instances.length-1; c++) {
+						instances[c][((accessToggle = !accessToggle) ? accessOrder[0] : accessOrder[1])][indexGroup[i]] = curPath;
+					}
+				}
+				break PATHFINDER;
 			}
-			
-			
-			
-			
-			
-//			int index = path.size() - 1;
-//			Vec2 coord = path.get(index--);
-//			
-//			PathInstance pi = new PathInstance((NodeCellComponent)(sheet[coord.getY()][coord.getX()]));
-//			
-//			List<Entry<Direction, List<PathCellComponent>>> flow = new ArrayList<>();
-//			
-//			do {
-//				Vec2 nextCoord = path.get(index--);
-//				List<PathCellComponent> vector = new ArrayList<>();
-//				Direction compass;
-//				int[] curPos = coord.getOrigin();
-//				int[] endPos = nextCoord.getOrigin();
-//				int[] mod;
-//				
-//				switch(coord.directionTo(nextCoord)) {
-//				case UP:
-//					compass = Direction.UP;
-//					mod = new int[] {0, -1};
-//					break;
-//				case DOWN:
-//					compass = Direction.DOWN;
-//					mod = new int[] {0, 1};
-//					break;
-//				case LEFT:
-//					compass = Direction.LEFT;
-//					mod = new int[] {0, -1};
-//					break;
-//				case RIGHT:
-//					compass = Direction.RIGHT;
-//					mod = new int[] {0, 1};
-//					break;
-//				default:
-//					System.out.println("Something went wrong finding the next direction");
-//					return;
-//				}
-//				
-//				while(curPos[0] != endPos[0] && curPos[1] != endPos[1]) {
-//					if(!(sheet[curPos[1]][curPos[0]] instanceof NodeCellComponent)) {
-//						if(!(sheet[curPos[1]][curPos[0]] instanceof PathCellComponent)) {
-//							sheet[curPos[1]][curPos[0]] = new PathCellComponent(newCellWidth, newCellHeight, xChannels, yChannels);
-//						}
-//						vector.add((PathCellComponent)(sheet[curPos[1]][curPos[0]]));
-//					}
-//					curPos[0] += mod[0];
-//					curPos[1] += mod[1];
-//				} 
-//				
-//				coord = nextCoord;
-//				flow.add(new SimpleEntry<>(compass, vector));
-//			} while(index >= 0);
-//			
-//			/**
-//			 * Above logic should result an instruction and traversal compilation
-//			 * Following logic must traverse the PathCell along relevant axis and
-//			 * infer the non occupied lane of travel to inhabit.
-//			 */
-//			
-//			for(Entry<Direction, List<PathCellComponent>> instruction : flow) {
-//				//TODO trying impl without making multiple collections
-//			}
-			
-			
-			
-		}
+		} System.out.println("worked?");
 	}
+	
 	
 	/**
 	 * Draw reference utilized in incorporating classes.
@@ -169,7 +183,7 @@ public class LinkedPath {
 	 * @author viney
 	 *
 	 */
-	class PathInstance {
+	static class PathInstance {
 		
 		private Color pathColor;
 		
